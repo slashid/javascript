3b1b8aef998fa30bf62b1b4bace9f40ed22a21ea
ikovic <kovic.ivan@gmail.com>
2022-10-27 15:47:40 +0200

    Build the library as ESM and output type declarations

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -4,20 +4,17 @@ import react from "@vitejs/plugin-react";
 
 import * as packageJson from "./package.json";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
-      // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, "src/main.ts"),
-      name: "@slashid/react",
-      // the proper extensions will be added
-      formats: ["es", "umd"],
-      fileName: (format) => `slashid-react.${format}.js`,
+      formats: ["es"],
+      fileName: `main`,
     },
     rollupOptions: {
       external: [...Object.keys(packageJson.peerDependencies)],
     },
+    sourcemap: true,
   },
 });
