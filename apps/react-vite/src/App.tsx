0845c80414001c9ec1a3321cfb6da399f33daee2
ikovic <kovic.ivan@gmail.com>
2022-11-09 17:41:37 +0100

    Expose the correct Factor type

--- apps/react-vite/src/App.tsx
+++ apps/react-vite/src/App.tsx
@@ -1,26 +1,29 @@
 import { useSlashID } from "@slashid/react";
+import { useCallback } from "react";
 import "./App.css";
 
 function App() {
-  const { user, logIn } = useSlashID();
+  const { user, logIn, logOut } = useSlashID();
+
+  const handleClick = useCallback(async () => {
+    if (user) {
+      await logOut();
+    } else {
+      await logIn({
+        handle: {
+          type: import.meta.env.VITE_IDENTIFIER_TYPE,
+          value: import.meta.env.VITE_IDENTIFIER_VALUE,
+        },
+        factor: { method: import.meta.env.VITE_AUTH_METHOD },
+      });
+    }
+  }, [logIn, logOut, user]);
 
   return (
     <div className="App">
       <h1>/id React SDK</h1>
       <div className="card">
-        <button
-          onClick={() =>
-            logIn({
-              handle: {
-                type: import.meta.env.VITE_IDENTIFIER_TYPE,
-                value: import.meta.env.VITE_IDENTIFIER_VALUE,
-              },
-              factor: { method: import.meta.env.VITE_AUTH_METHOD },
-            })
-          }
-        >
-          Log in
-        </button>
+        <button onClick={handleClick}>{user ? "Log out" : "Log in"}</button>
         <p>
           <code>{user && user.token}</code>
         </p>
