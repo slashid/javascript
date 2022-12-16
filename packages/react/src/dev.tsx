a57e0c3275defe83cc67e042e4a9bc106ad9544c
ikovic <kovic.ivan@gmail.com>
2022-12-16 14:04:13 +0100

    Add the background color

--- packages/react/src/dev.tsx
+++ packages/react/src/dev.tsx
@@ -4,6 +4,8 @@ import { Form } from "./components/form";
 import { ConfigurationProvider } from "./context/config-context";
 import { SlashIDProvider } from "./main";
 
+import "./dev.css";
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <SlashIDProvider oid={import.meta.env.VITE_ORG_ID}>
