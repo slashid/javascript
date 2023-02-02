439c94ef3aa7a693848f4bdb243c11ea4aa3094b
Kasper Mroz <kasper@slashid.dev>
2023-02-02 15:22:00 +0100

    missing import

--- packages/react/src/components/form/form.tsx
+++ packages/react/src/components/form/form.tsx
@@ -1,5 +1,6 @@
 import "@fontsource/inter/400.css";
 import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
 
 import { clsx } from "clsx";
 import { useFlowState } from "./useFlowState";
