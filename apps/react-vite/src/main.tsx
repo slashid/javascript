2520515d0ed4ff93cf1e88693d9cbb62b00b4fdd
Kasper Mroz <kasper@slashid.dev>
2023-02-22 09:36:26 +0100

    fix styles in react vite app

--- apps/react-vite/src/main.tsx
+++ apps/react-vite/src/main.tsx
@@ -3,12 +3,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "demo-form/style.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <SlashIDProvider
       oid={import.meta.env.VITE_ORG_ID}
       tokenStorage="localStorage"
+      baseApiUrl="https://api.sandbox.slashid.com"
     >
       <App />
     </SlashIDProvider>
