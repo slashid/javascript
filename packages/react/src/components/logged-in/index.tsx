486dfe038be489463b50b6a0ef17c4928b0b0736
Kasper Mroz <kasper@slashid.dev>
2023-03-17 17:12:56 +0100

    <LoggedIn> API extension proposal

--- packages/react/src/components/logged-in/index.tsx
+++ packages/react/src/components/logged-in/index.tsx
@@ -1,16 +1,52 @@
-import { useSlashID } from "../../main";
+import type { Factor, User } from "@slashid/slashid";
+import { useMemo } from "react";
+import { useSlashID } from "../../hooks/use-slash-id";
+
+type FactorMethods = Factor["method"][];
 
 type Props = {
   children: React.ReactNode;
+  withFactorMethods?:
+    | FactorMethods
+    | ((userAuthenticationMethods: FactorMethods) => boolean);
+};
+
+const hasUserAuthenticationMethods = (user: User, methods: FactorMethods) => {
+  for (const factorMethod of methods) {
+    if (!user.authentication.includes(factorMethod)) {
+      return false;
+    }
+  }
+
+  return true;
 };
 
 /**
  * Renders the children only when the SDK is ready and the user is authenticated.
  */
-export const LoggedIn: React.FC<Props> = ({ children }) => {
+export const LoggedIn: React.FC<Props> = ({ children, withFactorMethods }) => {
   const { user } = useSlashID();
 
-  if (!user) {
+  const shouldRender = useMemo(() => {
+    // user must be logged in
+    if (!user) {
+      return false;
+    }
+
+    // user must be authenticated with all factor methods
+    if (withFactorMethods && Array.isArray(withFactorMethods)) {
+      return hasUserAuthenticationMethods(user, withFactorMethods);
+    }
+
+    // callback function must return `true`
+    if (withFactorMethods && typeof withFactorMethods === "function") {
+      return withFactorMethods(user.authentication);
+    }
+
+    return true;
+  }, [user, withFactorMethods]);
+
+  if (!shouldRender) {
     return null;
   }
 
