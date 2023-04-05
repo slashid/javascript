85eda8b293874ec3f4e927376b4d856866569acd
ikovic <kovic.ivan@gmail.com>
2023-04-05 17:21:22 +0200

    Test SSR

--- apps/react-remix/app/routes/index.tsx
+++ apps/react-remix/app/routes/index.tsx
@@ -1,16 +1,30 @@
-import type { LinksFunction } from "@remix-run/node";
+import { LinksFunction, json } from "@remix-run/node";
 import { LoggedOut, LoggedIn, Form } from "@slashid/react";
+import { User } from "@slashid/slashid/ssr";
 import { Profile } from "demo-form";
 
 import slashIDstyles from "@slashid/react/style.css";
 import profileStyles from "demo-form/style.css";
+import { useLoaderData } from "@remix-run/react";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: slashIDstyles },
   { rel: "stylesheet", href: profileStyles },
 ];
 
+export async function loader() {
+  const ssrUser = new User(
+    "eyJhbGciOiJSUzI1NiIsICJraWQiOiJwWXNOR0EifQ.eyJhdWQiOiI4MTdiMWQyOS0zODAwLWJjMGMtMzZlYi04M2FiNjMyZjY2YTUiLCAiYXV0aGVudGljYXRlZF9tZXRob2RzIjpbImVtYWlsX2xpbmsiXSwgImV4cCI6MTY4MDc5Mjc0NiwgImZpcnN0X3Rva2VuIjpmYWxzZSwgImdyb3VwcyI6WyJUZXN0LTEiXSwgImdyb3Vwc19jbGFpbV9uYW1lIjoiZ3JvdXBzIiwgImlhdCI6MTY4MDcwNjM0NiwgImlzcyI6Imh0dHBzOi8vYXBpLnNhbmRib3guc2xhc2hpZC5jb20iLCAianRpIjoiZjdiNDI2MDc1NTVjNDAyNTJhMTY0OTI4OTllODlhMGYiLCAib2lkIjoiODE3YjFkMjktMzgwMC1iYzBjLTM2ZWItODNhYjYzMmY2NmE1IiwgInBlcnNvbl9pZCI6IjA2M2U0ZDI4LWY1OGQtN2JiNC1iYzA4LTA4ZDg3MmY4MGU4OSIsICJzdWIiOiIwNjNlNGQyOC1mNThkLTdiYjQtYmMwOC0wOGQ4NzJmODBlODkifQ.P7Ot32_a-W6S7_Qh7DauqNFrrIYxmLNVEM-ZBfga6sx_N9OR3W2Aj_c1UdGjWjUPTxskpBMGmczimMiWfk6-bnFv5_23BB-RRnE9ovQzVpVjGv2R6akm58nYZ4EcF-FMQMDR86wzCgeFfuWNNIiylc63UWQLOVj_0qLxj141M_rPyBsoYGDfDoEM4W1IIlAkjF1fGH68doPZ0vu8I3Bw2dwv7CmUl8PaY_pAgN9a_LhOljwAtA6u_W1of3DfszNx-v7lHmP8ptLdA_gLapvy6_NNphx0Srq1tS637pDjETjGTNMGtoNOwenf6R5Odto7yLkaJCxnxdoJuG0M8Zg50w"
+  );
+  const bucket = await ssrUser.getBucket();
+  const attributes = await bucket.get();
+  return json({ attributes });
+}
+
 export default function Index() {
+  const { attributes } = useLoaderData<typeof loader>();
+  console.log({ attributes });
+
   return (
     <div className="index">
       <LoggedOut>
--- apps/react-remix/app/slash-id.tsx
+++ apps/react-remix/app/slash-id.tsx
@@ -1,7 +1,6 @@
 import type { Factor } from "@slashid/slashid";
 import { ReactNode, useEffect } from "react";
 import { SlashIDProvider, ConfigurationProvider } from "@slashid/react";
-import { User } from "@slashid/slashid/ssr";
 
 type Props = {
   children: ReactNode;
@@ -9,20 +8,6 @@ type Props = {
 
 export const SlashID = ({ children }: Props) => {
   const factors: Factor[] = [{ method: "email_link" }];
-
-  useEffect(() => {
-    const ssrUser = new User(
-      "eyJhbGciOiJSUzI1NiIsICJraWQiOiJwWXNOR0EifQ.eyJhdWQiOiI4MTdiMWQyOS0zODAwLWJjMGMtMzZlYi04M2FiNjMyZjY2YTUiLCAiYXV0aGVudGljYXRlZF9tZXRob2RzIjpbImVtYWlsX2xpbmsiXSwgImV4cCI6MTY4MDc5Mjc0NiwgImZpcnN0X3Rva2VuIjpmYWxzZSwgImdyb3VwcyI6WyJUZXN0LTEiXSwgImdyb3Vwc19jbGFpbV9uYW1lIjoiZ3JvdXBzIiwgImlhdCI6MTY4MDcwNjM0NiwgImlzcyI6Imh0dHBzOi8vYXBpLnNhbmRib3guc2xhc2hpZC5jb20iLCAianRpIjoiZjdiNDI2MDc1NTVjNDAyNTJhMTY0OTI4OTllODlhMGYiLCAib2lkIjoiODE3YjFkMjktMzgwMC1iYzBjLTM2ZWItODNhYjYzMmY2NmE1IiwgInBlcnNvbl9pZCI6IjA2M2U0ZDI4LWY1OGQtN2JiNC1iYzA4LTA4ZDg3MmY4MGU4OSIsICJzdWIiOiIwNjNlNGQyOC1mNThkLTdiYjQtYmMwOC0wOGQ4NzJmODBlODkifQ.P7Ot32_a-W6S7_Qh7DauqNFrrIYxmLNVEM-ZBfga6sx_N9OR3W2Aj_c1UdGjWjUPTxskpBMGmczimMiWfk6-bnFv5_23BB-RRnE9ovQzVpVjGv2R6akm58nYZ4EcF-FMQMDR86wzCgeFfuWNNIiylc63UWQLOVj_0qLxj141M_rPyBsoYGDfDoEM4W1IIlAkjF1fGH68doPZ0vu8I3Bw2dwv7CmUl8PaY_pAgN9a_LhOljwAtA6u_W1of3DfszNx-v7lHmP8ptLdA_gLapvy6_NNphx0Srq1tS637pDjETjGTNMGtoNOwenf6R5Odto7yLkaJCxnxdoJuG0M8Zg50w"
-    );
-
-    async function doAsync() {
-      const bucket = await ssrUser.getBucket();
-      const attributes = await bucket.get();
-      console.log({ attributes });
-    }
-
-    doAsync();
-  }, []);
 
   return (
     <SlashIDProvider
