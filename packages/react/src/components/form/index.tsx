fc0f9afaf70621dd5e45cfe3469794690b0e0735
Kasper Mroz <kasper@slashid.dev>
2023-04-12 10:14:25 +0200

    Exclude font from the bundle

--- packages/react/src/components/form/index.tsx
+++ packages/react/src/components/form/index.tsx
@@ -1,6 +1,2 @@
-import "@fontsource/inter/400.css";
-import "@fontsource/inter/500.css";
-import "@fontsource/inter/600.css";
-
 export { Form } from "./form";
-export type { Props as FormProps  } from "./form";
+export type { Props as FormProps } from "./form";
