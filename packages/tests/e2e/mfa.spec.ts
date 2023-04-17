49cf2228672990172110d8066f29060c43de6745
Kasper Mróz <kaspermroz1234@gmail.com>
2023-04-17 15:45:16 +0200

    Make tests package private (#73)
    * Make tests package private
    
    * changeset

--- packages/tests/e2e/mfa.spec.ts
+++ packages/tests/e2e/mfa.spec.ts
@@ -1,5 +1,4 @@
 import { test, expect } from "@playwright/test";
-import { request } from "http";
 
 test("load <MultiFactorAuth> demo page", async ({ page }) => {
   await page.goto("http://localhost:3000/mfa");
