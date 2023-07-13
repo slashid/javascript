7ede2f1e448d83eb2f8a3b370da2a3c37cc46f91
ikovic <kovic.ivan@gmail.com>
2023-07-13 18:33:58 +0200

    Start the flow dynamically

--- packages/react/src/components/dynamic-flow/index.tsx
+++ packages/react/src/components/dynamic-flow/index.tsx
@@ -6,12 +6,18 @@ import * as styles from "../form/form.css";
 import { HandleForm, Logo } from "../form/initial";
 import { Text } from "../text";
 import { FormProvider } from "../../context/form-context";
-import { useCallback, useState } from "react";
+import { useCallback } from "react";
 import { Handle } from "../../domain/types";
+import { CreateFlowOptions } from "../form/flow";
+import { useFlowState } from "../form/useFlowState";
+import { Authenticating } from "../form/authenticating";
+import { Success } from "../form/success";
+import { Error } from "../form/error";
 
 type Props = {
   className?: string;
-  getFactors: (email: string) => Factor[];
+  onSuccess?: CreateFlowOptions["onSuccess"];
+  getFactor: (email: string) => Factor;
 };
 
 type FormProps = {
@@ -38,39 +44,57 @@ const FormWrapper = ({ children, className }: FormProps) => {
   );
 };
 
-export const DynamicFlow = ({ getFactors, className }: Props) => {
+export const DynamicFlow = ({ getFactor, className, onSuccess }: Props) => {
   const { logo } = useConfiguration();
-  const [handle, setHandle] = useState<string>("");
-  const [factors, setFactors] = useState<Factor[]>([]);
+  const flowState = useFlowState({ onSuccess });
 
   const setHandleAndFactors = useCallback(
     (factor: Factor, handle: Handle) => {
-      setHandle(handle.value);
-      setFactors(getFactors(handle.value));
+      if (flowState.status === "initial") {
+        const factor = getFactor(handle.value);
+        console.log({ factor, handle });
+        flowState.logIn({
+          factor,
+          handle,
+        });
+      }
     },
-    [getFactors]
+    [getFactor, flowState]
   );
-
-  console.log({ handle, factors });
 
   return (
     <div>
       <FormWrapper className={className}>
         <article data-testid="sid-dynamic-flow--initial-state">
-          <Logo logo={logo} />
-          <Text
-            as="h1"
-            variant={{ size: "2xl-title", weight: "bold" }}
-            t="initial.title"
-          />
-          <Text variant={{ color: "tertiary" }} as="h2" t="initial.subtitle" />
-          <FormProvider>
-            <HandleForm
-              handleType="email_address"
-              factors={[]}
-              handleSubmit={setHandleAndFactors}
-            />
-          </FormProvider>
+          {flowState.status === "initial" && (
+            <>
+              <Logo logo={logo} />
+              <Text
+                as="h1"
+                variant={{ size: "2xl-title", weight: "bold" }}
+                t="initial.title"
+              />
+              <Text
+                variant={{ color: "tertiary" }}
+                as="h2"
+                t="initial.subtitle"
+              />
+              <FormProvider>
+                <HandleForm
+                  handleType="email_address"
+                  factors={[]}
+                  handleSubmit={setHandleAndFactors}
+                />
+              </FormProvider>
+            </>
+          )}
+          {flowState.status === "authenticating" && (
+            <FormProvider>
+              <Authenticating flowState={flowState} />
+            </FormProvider>
+          )}
+          {flowState.status === "error" && <Error flowState={flowState} />}
+          {flowState.status === "success" && <Success flowState={flowState} />}
         </article>
       </FormWrapper>
     </div>
