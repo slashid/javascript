918d038e50b7224c67ba5257dc3cfa0ad07302c2
Ahmed Gazimi <ahmed.gazimi@gmail.com>
2023-08-03 20:51:27 +0100

    Add Switch component

---
+++ packages/react/src/components/switch/style.css.ts
@@ -0,0 +1,55 @@
+import { style } from "@vanilla-extract/css";
+import { colors, publicVariables, theme } from "../../theme/theme.css";
+
+export const radixSwitch = style({
+  all: "unset",
+  position: "relative",
+  display: "block",
+  width: "34px",
+  height: "24px",
+  borderRadius: "100px",
+
+  selectors: {
+    '&[data-state="unchecked"]': {
+      backgroundColor: publicVariables.color.placeholder,
+    },
+    '&[data-state="unchecked"]:hover': {
+      backgroundColor: publicVariables.color.tertiary,
+    },
+    '&[data-state="checked"]': {
+      backgroundColor: publicVariables.color.primary,
+    },
+    '&[data-state="checked"]:hover': {
+      backgroundColor: publicVariables.color.primaryHover,
+    },
+    '&[data-blocked="true"]': {
+      backgroundColor: publicVariables.color.subtle,
+    },
+    '&[data-blocked="true"]:hover': {
+      backgroundColor: publicVariables.color.subtle,
+    },
+  },
+});
+
+export const switchThumb = style({
+  display: "block",
+  width: "20px",
+  height: "20px",
+  margin: "2px",
+  backgroundColor: colors.white,
+  borderRadius: "100px",
+  transition: "transform 100ms",
+  willChange: "transform",
+
+  selectors: {
+    '&[data-state="checked"]': {
+      transform: "translateX(10px)",
+      boxShadow: theme.boxShadow.dim,
+    },
+  },
+});
+
+export const switchLock = style({
+  display: "block",
+  margin: "auto",
+});
