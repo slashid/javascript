8c57eb4a88cf4b3f3723ced501e4591fcd617289
Jake Whelan <jake@slashid.dev>
2023-08-09 15:26:26 +0100

    context

--- packages/react/src/middleware/index.ts
+++ packages/react/src/middleware/index.ts
@@ -1,7 +1,8 @@
-import { LoginMiddleware } from "../domain/types"
+import { LoginMiddleware, LoginMiddlewareContext } from "../domain/types"
 import { defaultOrganization } from "./default-organization"
 
 export {
   defaultOrganization,
-  type LoginMiddleware
+  type LoginMiddleware,
+  type LoginMiddlewareContext
 }
