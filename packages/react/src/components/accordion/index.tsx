ecb084ac6bd9e5931d9170756237c7aa976634f8
Ahmed Gazimi <ahmed.gazimi@gmail.com>
2023-08-18 15:45:00 +0100

    Add JSDocs

--- packages/react/src/components/accordion/index.tsx
+++ packages/react/src/components/accordion/index.tsx
@@ -5,18 +5,29 @@ import { ChevronLeft } from "../icon/chevron-left";
 import * as styles from "./style.css";
 
 type Item = {
+  /** Unique value of the item */
   value: string;
+  /** Icon to display next to the trigger */
   icon?: ReactNode;
+  /** Trigger TO Toggle the collapsed state of its associated item */
   trigger: ReactNode;
+  /** Content to display when the item is expanded */
   content: ReactNode;
 };
 
 type Props = {
+  /** List of items to display */
   items: Item[];
+  /** Custom class name for the accordion */
   className?: string;
+  /** Custom class name for each item */
   itemClassName?: string;
 };
 
+/**
+ * Accordion component built on top of Radix Accordion primitives to display a list of items
+ * that can be expanded and collapsed. It supports multiple items expanded at the same time.
+ */
 export const Accordion = ({ items, className, itemClassName }: Props) => (
   <RadixAccordion.Root
     className={clsx("sid-accordion", className)}
--- packages/react/src/components/switch/index.tsx
+++ packages/react/src/components/switch/index.tsx
@@ -4,9 +4,14 @@ import * as RadixSwitch from "@radix-ui/react-switch";
 import { Lock } from "../icon/lock";
 
 type Props = RadixSwitch.SwitchProps & {
+  /** blocked state where the switch is disabled and a lock icon is displayed */
   blocked?: boolean;
 };
 
+/**
+ * Switch component built on top of Radix Switch primitives to display a toggle button.
+ * It supports a blocked state where the switch is disabled and a lock icon is displayed.
+ */
 export const Switch = ({ className, blocked = false, ...props }: Props) => (
   <RadixSwitch.Root
     className={clsx("sid-switch", styles.radixSwitch, className)}
