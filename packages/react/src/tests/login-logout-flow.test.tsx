b7d8f4f50bb1ed401c1563590cbf4740f65aec8b
Kasper Mroz <kasper@slashid.dev>
2023-08-22 12:32:45 +0200

    wrap tests in act()

--- packages/react/src/tests/login-logout-flow.test.tsx
+++ packages/react/src/tests/login-logout-flow.test.tsx
@@ -1,7 +1,8 @@
 import type { PropsWithChildren } from "react";
 import { describe, it } from "vitest";
-import { renderHook } from "@testing-library/react";
+import { renderHook, waitFor, act } from "@testing-library/react";
 import { useSlashID, SlashIDProvider } from "../main";
+import { wait } from "@testing-library/user-event/dist/types/utils";
 
 describe("Log in / Log out flow", () => {
   it("should log in correctly after logging out", async () => {
@@ -10,37 +11,107 @@ describe("Log in / Log out flow", () => {
       <SlashIDProvider oid={TEST_ROOT_OID}>{children}</SlashIDProvider>
     );
 
-    const { result, rerender } = renderHook(() => useSlashID(), { wrapper });
-
-    const user = await result.current.logIn({
-      factor: {
-        method: "email_link",
-      },
-      handle: {
-        type: "email_address",
-        value: "test@example.com",
-      },
+    const { result, unmount } = renderHook(() => useSlashID(), {
+      wrapper,
     });
-    expect(user?.token).toBeDefined();
-    rerender();
-    expect(result.current.user).toEqual(user);
+    await act(
+      () =>
+        async function () {
+          const user = await result.current.logIn({
+            factor: {
+              method: "email_link",
+            },
+            handle: {
+              type: "email_address",
+              value: "test@example.com",
+            },
+          });
+          expect(user?.token).toBeDefined();
+          await waitFor(() => expect(result.current.user).toEqual(user));
+        }
+    );
 
     result.current.logOut();
-    rerender();
-    expect(result.current.user).toBeUndefined();
+    await waitFor(() => expect(result.current.user).toBeUndefined());
 
-    const newUser = await result.current.logIn({
-      factor: {
-        method: "email_link",
-      },
-      handle: {
-        type: "email_address",
-        value: "test2@example.com",
-      },
-    });
+    await act(
+      () =>
+        async function () {
+          const newUser = await result.current.logIn({
+            factor: {
+              method: "email_link",
+            },
+            handle: {
+              type: "email_address",
+              value: "test2@example.com",
+            },
+          });
 
-    expect(newUser?.token).toBeDefined();
-    rerender();
-    expect(result.current.user).toEqual(newUser);
+          expect(newUser?.token).toBeDefined();
+          await waitFor(() => expect(result.current.user).toEqual(newUser));
+        }
+    );
+    unmount();
+  });
+
+  it("should have root oid after org switching and logOut", async () => {
+    const TEST_ROOT_OID = "oid_1";
+    const wrapper = ({ children }: PropsWithChildren<any>) => (
+      <SlashIDProvider oid={TEST_ROOT_OID}>{children}</SlashIDProvider>
+    );
+
+    const { result, unmount } = renderHook(() => useSlashID(), { wrapper });
+
+    await act(
+      () =>
+        async function () {
+          const user = await result.current.logIn({
+            factor: {
+              method: "email_link",
+            },
+            handle: {
+              type: "email_address",
+              value: "test@example.com",
+            },
+          });
+          expect(user?.token).toBeDefined();
+          await waitFor(() => expect(result.current.user).toEqual(user));
+        }
+    );
+
+    await act(
+      () =>
+        async function () {
+          await result.current.__switchOrganizationInContext({
+            oid: "other_oid",
+          });
+
+          await waitFor(() =>
+            expect(result.current.user?.oid).toEqual("other_oid")
+          );
+        }
+    );
+
+    result.current.logOut();
+    await waitFor(() => expect(result.current.user).toBeUndefined());
+
+    await act(
+      () =>
+        async function () {
+          const newUser = await result.current.logIn({
+            factor: {
+              method: "email_link",
+            },
+            handle: {
+              type: "email_address",
+              value: "test2@example.com",
+            },
+          });
+
+          expect(newUser?.token).toBeDefined();
+          await waitFor(() => expect(newUser?.oid).toEqual(TEST_ROOT_OID));
+        }
+    );
+    unmount();
   });
 });
