0398b56445c69d2913eff05c0358285a637a4475
ikovic <kovic.ivan@gmail.com>
2023-09-14 11:08:16 +0200

    Silence the SSR warning for ThemeRoot

--- packages/react/src/components/theme-root/index.tsx
+++ packages/react/src/components/theme-root/index.tsx
@@ -7,6 +7,8 @@ import {
   themeClass,
 } from "../../theme/theme.css";
 import { useLayoutEffect } from "react";
+import React from "react";
+import { isBrowser } from "../../browser/is-browser";
 
 export type ThemeProps = {
   theme?: Theme;
@@ -19,27 +21,55 @@ type Props = {
   className?: ThemeProps["className"];
 };
 
+function createClassNames({ theme, className }: ThemeProps) {
+  return clsx(
+    "sid-theme-root",
+    `sid-theme-root__${theme}`,
+    themeClass,
+    {
+      [darkTheme]: theme === "dark",
+      [autoTheme]: theme === "auto",
+      [lightThemeVars]: theme === "light",
+    },
+    className
+  );
+}
+
+function setThemeRootClassNames({ theme, className }: ThemeProps) {
+  const themeRootClassNames = createClassNames({ theme, className });
+
+  document.body.classList.add(...themeRootClassNames.split(" "));
+}
+
+/**
+ * We cannot rely on useLayoutEffect to set the correct class names on the <body> element server side.
+ */
+const ServerThemeRoot = ({ children }: Props) => {
+  return <>{children}</>;
+};
+
+const ClientThemeRoot = ({ children, theme = "light", className }: Props) => {
+  // this effect runs client side only
+  useLayoutEffect(() => {
+    setThemeRootClassNames({ theme, className });
+  }, [className, theme]);
+
+  return <>{children}</>;
+};
+
 /**
  * This component will apply the necessary class names to the <body> element.
  * That way the child components on all levels can use the theming properties.
  * This also helps with any components portaling out of the SlashIDProvider hierarchy.
  */
 export function ThemeRoot({ children, theme = "light", className }: Props) {
-  useLayoutEffect(() => {
-    const themeRootClassNames = clsx(
-      "sid-theme-root",
-      `sid-theme-root__${theme}`,
-      themeClass,
-      {
-        [darkTheme]: theme === "dark",
-        [autoTheme]: theme === "auto",
-        [lightThemeVars]: theme === "light",
-      },
-      className
-    );
+  if (!isBrowser()) {
+    return <ServerThemeRoot>{children}</ServerThemeRoot>;
+  }
 
-    document.body.classList.add(...themeRootClassNames.split(" "));
-  }, [className, theme]);
-
-  return <>{children}</>;
+  return (
+    <ClientThemeRoot theme={theme} className={className}>
+      {children}
+    </ClientThemeRoot>
+  );
 }
