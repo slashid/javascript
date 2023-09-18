771294c21cd2a3f12ffff094f1a3f8014c2ed769
Ahmed Gazimi <ahmed.gazimi@gmail.com>
2023-09-18 11:40:38 +0100

    Update form stories

--- packages/react/src/components/form/form.stories.tsx
+++ packages/react/src/components/form/form.stories.tsx
@@ -1,11 +1,11 @@
+import { Factor } from "@slashid/slashid";
 import { Meta, StoryObj } from "@storybook/react";
+import { useState } from "react";
 import { Form } from ".";
 import { ConfigurationProvider } from "../../context/config-context";
+import { Handle } from "../../domain/types";
 import { defaultOrganization } from "../../middleware";
 import { Slot } from "../slot";
-import { Factor } from "@slashid/slashid";
-import { useState } from "react";
-import { Handle } from "../../domain/types";
 
 const meta: Meta<typeof Form> = {
   component: Form,
@@ -89,40 +89,44 @@ export const WithMiddleware: Story = {
 export const WithSlots: Story = {
   name: "With Slots API",
   render: () => (
-    <ConfigurationProvider>
-      <p>Using layout slots to customise the form</p>
-      <Form>
-        <Slot name="footer">
-          <footer>Custom footer</footer>
-        </Slot>
-      </Form>
-    </ConfigurationProvider>
+    <div style={{ maxWidth: 450, margin: "auto" }}>
+      <ConfigurationProvider>
+        <p>Using layout slots to customise the form</p>
+        <Form>
+          <Slot name="footer">
+            <footer>Custom footer</footer>
+          </Slot>
+        </Form>
+      </ConfigurationProvider>
+    </div>
   ),
 };
 
 export const WithComposition: Story = {
   name: "With Slots API and composition",
   render: () => (
-    <ConfigurationProvider>
-      <p>
-        Using the initial slot to customise the form layout using the building
-        blocks we provide
-      </p>
-      <Form>
-        <Slot name="initial">
-          <Form.Initial.Logo />
-          <Form.Initial.Header />
-          <p>Components can be added anywhere to the initial slot</p>
-          <Form.Initial.Controls>
-            <Form.Initial.Controls.Input />
-            <div data-testid="custom-text-controls">
-              Arbitrary components can within the form controls
-            </div>
-            <Form.Initial.Controls.Submit />
-          </Form.Initial.Controls>
-        </Slot>
-      </Form>
-    </ConfigurationProvider>
+    <div style={{ maxWidth: 450, margin: "auto" }}>
+      <ConfigurationProvider>
+        <p>
+          Using the initial slot to customise the form layout using the building
+          blocks we provide
+        </p>
+        <Form>
+          <Slot name="initial">
+            <Form.Initial.Logo />
+            <Form.Initial.Header />
+            <p>Components can be added anywhere to the initial slot</p>
+            <Form.Initial.Controls>
+              <Form.Initial.Controls.Input />
+              <div data-testid="custom-text-controls">
+                Arbitrary components can within the form controls
+              </div>
+              <Form.Initial.Controls.Submit />
+            </Form.Initial.Controls>
+          </Slot>
+        </Form>
+      </ConfigurationProvider>
+    </div>
   ),
 };
 
@@ -158,20 +162,22 @@ const ComposedForm = ({
 export const WithCompositionAndCustomisation: Story = {
   name: "With custom building blocks",
   render: () => (
-    <ConfigurationProvider>
-      <p>Replacing the form building blocks with custom components</p>
-      <Form>
-        <Slot name="initial">
-          <Form.Initial.Controls>
-            {({ handleSubmit }) => {
-              return <ComposedForm handleSubmit={handleSubmit} />;
-            }}
-          </Form.Initial.Controls>
-        </Slot>
-        <Slot name="success">
-          <div data-testid="custom-success">Custom success</div>
-        </Slot>
-      </Form>
-    </ConfigurationProvider>
+    <div style={{ maxWidth: 450, margin: "auto" }}>
+      <ConfigurationProvider>
+        <p>Replacing the form building blocks with custom components</p>
+        <Form>
+          <Slot name="initial">
+            <Form.Initial.Controls>
+              {({ handleSubmit }) => {
+                return <ComposedForm handleSubmit={handleSubmit} />;
+              }}
+            </Form.Initial.Controls>
+          </Slot>
+          <Slot name="success">
+            <div data-testid="custom-success">Custom success</div>
+          </Slot>
+        </Form>
+      </ConfigurationProvider>
+    </div>
   ),
 };
