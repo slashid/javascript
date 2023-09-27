278d1ba6769f56ca4490d292644b63823baaa2b3
Ahmed Gazimi <ahmed.gazimi@gmail.com>
2023-09-27 14:36:03 +0100

    Move getAuthenticatingMessage

--- packages/react/src/components/form/authenticating.tsx
+++ packages/react/src/components/form/authenticating.tsx
@@ -7,7 +7,6 @@ import {
   useState,
 } from "react";
 import {
-  getAuthenticatingMessage,
   isFactorEmailLink,
   isFactorOTP,
   isFactorOTPEmail,
@@ -25,10 +24,64 @@ import { OtpInput } from "../otp-input";
 import { Circle } from "../spinner/circle";
 import { Spinner } from "../spinner/spinner";
 import { Text } from "../text";
+import { TextConfigKey } from "../text/constants";
 import * as styles from "./authenticating.css";
 import { ErrorMessage } from "./error-message";
 import { AuthenticatingState } from "./flow";
 import { OTP_CODE_LENGTH, isValidOTPCode } from "./validation";
+
+function getAuthenticatingMessage(
+  factor: Factor,
+  isSubmitting = false
+): { title: TextConfigKey; message: TextConfigKey } {
+  switch (factor.method) {
+    case "oidc":
+      return {
+        message: "authenticating.message.oidc",
+        title: "authenticating.title.oidc",
+      };
+    case "webauthn":
+      return {
+        message: "authenticating.message.webauthn",
+        title: "authenticating.title.webauthn",
+      };
+
+    case "sms_link":
+      return {
+        message: "authenticating.message.smsLink",
+        title: "authenticating.title.smsLink",
+      };
+    case "otp_via_sms": {
+      if (isSubmitting) {
+        return {
+          message: "authenticating.submitting.message.smsOtp",
+          title: "authenticating.submitting.title.smsOtp",
+        };
+      }
+      return {
+        message: "authenticating.message.smsOtp",
+        title: "authenticating.title.smsOtp",
+      };
+    }
+    case "otp_via_email":
+      if (isSubmitting) {
+        return {
+          message: "authenticating.submitting.message.emailOtp",
+          title: "authenticating.submitting.title.emailOtp",
+        };
+      }
+      return {
+        message: "authenticating.message.emailOtp",
+        title: "authenticating.title.emailOtp",
+      };
+    case "email_link":
+    default:
+      return {
+        message: "authenticating.message.emailLink",
+        title: "authenticating.title.emailLink",
+      };
+  }
+}
 
 const Loader = () => (
   <Circle>
--- packages/react/src/domain/handles.ts
+++ packages/react/src/domain/handles.ts
@@ -3,7 +3,6 @@ import {
   findFlagByDialCode,
   getList,
 } from "country-list-with-dial-code-and-flag";
-import { TextConfigKey } from "../components/text/constants";
 import {
   FactorEmailLink,
   FactorNonOIDC,
@@ -114,62 +113,5 @@ export function parsePhoneNumber(
         countryCode: findFlagByDialCode(flag.dial_code).code,
       };
     }
-  }
-}
-
-export type AuthenticatingMessage = {
-  title: TextConfigKey;
-  message: TextConfigKey;
-};
-export function getAuthenticatingMessage(
-  factor: Factor,
-  isSubmitting = false
-): AuthenticatingMessage {
-  switch (factor.method) {
-    case "oidc":
-      return {
-        message: "authenticating.message.oidc",
-        title: "authenticating.title.oidc",
-      };
-    case "webauthn":
-      return {
-        message: "authenticating.message.webauthn",
-        title: "authenticating.title.webauthn",
-      };
-
-    case "sms_link":
-      return {
-        message: "authenticating.message.smsLink",
-        title: "authenticating.title.smsLink",
-      };
-    case "otp_via_sms": {
-      if (isSubmitting) {
-        return {
-          message: "authenticating.submitting.message.smsOtp",
-          title: "authenticating.submitting.title.smsOtp",
-        };
-      }
-      return {
-        message: "authenticating.message.smsOtp",
-        title: "authenticating.title.smsOtp",
-      };
-    }
-    case "otp_via_email":
-      if (isSubmitting) {
-        return {
-          message: "authenticating.submitting.message.emailOtp",
-          title: "authenticating.submitting.title.emailOtp",
-        };
-      }
-      return {
-        message: "authenticating.message.emailOtp",
-        title: "authenticating.title.emailOtp",
-      };
-    case "email_link":
-    default:
-      return {
-        message: "authenticating.message.emailLink",
-        title: "authenticating.title.emailLink",
-      };
   }
 }
