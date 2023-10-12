3034eb7f0484a81cc912c67c1f23a7767306a0a1
Kasper Mroz <kasper@slashid.dev>
2023-10-12 11:09:34 +0200

    fix

--- packages/react/src/components/form/form.test.tsx
+++ packages/react/src/components/form/form.test.tsx
@@ -1,5 +1,5 @@
 import { SlashID, User } from "@slashid/slashid";
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { vi, describe, afterEach } from "vitest";
 import { Form } from ".";
