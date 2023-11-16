7eca76dd4623145aff94fc4c27f68fbc5d32f538
ikovic <kovic.ivan@gmail.com>
2023-11-16 16:33:36 +0100

    Remove unused files

--- packages/react-primitives/src/hooks/use-configuration.ts
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { ConfigurationContext } from "../context/config-context";
-
-export function useConfiguration() {
-  const contextValue = React.useContext(ConfigurationContext);
-
-  return contextValue;
-}
--- packages/react-primitives/src/hooks/use-form.ts
+++
@@ -1,4 +0,0 @@
-import { useContext } from "react";
-import { FormContext } from "../context/form-context";
-
-export const useForm = () => useContext(FormContext);
