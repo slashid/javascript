21183ff7987025967c4ce43892a6228ef5143f74
ikovic <kovic.ivan@gmail.com>
2023-11-21 17:30:56 +0100

    Prevent hydration errors

--- apps/jump-page/src/components/app/app.component.tsx
+++ apps/jump-page/src/components/app/app.component.tsx
@@ -1,14 +1,23 @@
 import { Logo, TextProvider, ThemeRoot } from "@slashid/react-primitives";
 import * as styles from "./app.css";
 import { AppContext, initialAppContextState } from "./app.context";
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { I18N, detectLanguage } from "../../domain/i18n";
 import { Flow } from "../flow";
 
 export function App() {
   const isBrowser = !!globalThis.window;
+  const [hydrated, setHydrated] = useState(false);
+
+  // used to prevent hydration mismatch
+  useEffect(() => {
+    setHydrated(true);
+  }, []);
+
   const config = useMemo(() => {
-    if (!isBrowser) {
+    // checking for hydration makes the first client side render the same as the one on the server side
+    // language check and URL params can only be done on the client side so we need to wait for the first render
+    if (!isBrowser || !hydrated) {
       return initialAppContextState;
     }
 
@@ -17,7 +26,7 @@ export function App() {
       logo: <Logo />,
       language: detectLanguage(),
     };
-  }, [isBrowser]);
+  }, [isBrowser, hydrated]);
 
   return (
     <ThemeRoot theme="light">
