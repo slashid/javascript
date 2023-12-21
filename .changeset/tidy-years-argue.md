64212fdef0c4fec1e1c9c52847324e9139328a5d
Ivan Ković <kovic.ivan@gmail.com>
2023-12-21 13:48:36 +0100

    [SID-1460] React SDK: do not expose the internal package (#194)
    * Use alias instead of internal package
    
    * Update package.json to point to the correct location for types
    
    * Add changeset
    
    * make react-primitives private
    
    * The default key needs to be the last in package.json exports in order for nextjs to work
    
    * Add the changeset
    
    * Share the aliases in test and build configs

---
+++ .changeset/tidy-years-argue.md
@@ -0,0 +1,5 @@
+---
+"@slashid/react": patch
+---
+
+Update the package so that it does not depend on react primitives on npm
--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -1,12 +1,11 @@
 import { resolve } from "node:path";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import { config } from "./vite.shared";
 
 import * as packageJson from "./package.json";
 
 export default defineConfig({
-  plugins: [react(), vanillaExtractPlugin()],
+  ...config,
   build: {
     lib: {
       entry: resolve(__dirname, "src/main.ts"),
