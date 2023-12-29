1f4f5e1c716f6a7ecfe1626efdcfb46fb5df4d95
Jake Whelan <jake@slashid.dev>
2023-12-29 17:50:14 +0000

    Expose groups

--- packages/remix/src/main.ts
+++ packages/remix/src/main.ts
@@ -1,6 +1,6 @@
 export { getUser } from "./util";
 export { type Factor, SSR } from "@slashid/slashid";
-export { LoggedIn, LoggedOut, ConfigurationProvider } from "@slashid/react";
+export { LoggedIn, LoggedOut, ConfigurationProvider, Groups } from "@slashid/react";
 export { createSlashIDApp } from "./slashid-app";
 export { useSlashID } from "./use-slash-id";
 export { Form } from "./form";
