206a1fe21eed85096e6b6e9c266086f16d0e69e5
ikovic <kovic.ivan@gmail.com>
2024-01-24 18:41:29 +0100

    Remove the jump page as it was moved to ng-evangelion

--- apps/jump-page/src/components/text/text.component.tsx
+++
@@ -1,8 +0,0 @@
-import { Text as BaseText, type TextProps } from "@slashid/react-primitives";
-import type { TranslationKeys } from "../../domain/i18n";
-
-type Props = TextProps<Record<TranslationKeys, string>>;
-
-export const Text: React.FC<Props> = (props) => {
-  return <BaseText {...props} />;
-};
--- apps/jump-page/src/config.ts
+++
@@ -1,7 +0,0 @@
-export const config = {
-  sdkURL:
-    import.meta.env.PUBLIC_SID_SDK_URL ||
-    "https://jump.slashid.local",
-  baseURL:
-    import.meta.env.PUBLIC_SID_API_URL || "https://slashid.local",
-};
