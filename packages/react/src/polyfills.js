9872ee30fdca112c980a9bc6fef0c1abbce70958
Jake Whelan <jake@slashid.dev>
2024-02-28 14:01:23 +0000

    polyfill Object.hasOwn

---
+++ packages/react/src/polyfills.js
@@ -0,0 +1,5 @@
+import hasOwn from "object.hasown";
+
+if (!Object.hasOwn) {
+  hasOwn.shim();
+}
--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -1,11 +1,15 @@
-import { resolve } from "node:path";
+import { resolve, join } from "node:path";
 import { defineConfig } from "vite";
 import { config } from "./vite.shared";
+import rollupPluginPolyfill from "rollup-plugin-polyfill";
 
 import * as packageJson from "./package.json";
 
+const polyfillsJs = resolve(join(__dirname, "src/polyfills.js"));
+
 export default defineConfig({
   ...config,
+  plugins: [rollupPluginPolyfill([polyfillsJs])],
   build: {
     lib: {
       entry: resolve(__dirname, "src/main.ts"),
