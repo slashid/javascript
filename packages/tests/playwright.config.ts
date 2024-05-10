2d6c010d827fecf42380a3dfc77d0b843952802d
Kasper Mroz <kasper@slashid.dev>
2024-05-10 14:25:25 +0200

    Record traces for local debugging

--- packages/tests/playwright.config.ts
+++ packages/tests/playwright.config.ts
@@ -39,7 +39,7 @@ function createPlaywrightConfig(): PlaywrightTestConfig {
       // baseURL: 'http://127.0.0.1:3000',
 
       /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-      trace: "on-first-retry",
+      trace: envConfig.CI ? "on-first-retry" : "on",
     },
 
     /* Configure projects for major browsers */
